5a75f38a1eaa19706efc3ae2c0f0240aa519acfa
bhardwajhimanshu2515@gmail.com <bhardwajhimanshu2515@gmail.com>
2021-02-23 12:39:41 +0530

    done

---
+++ Day 12 -Express+Mongo/back-end/server.js
@@ -0,0 +1,60 @@
+//import express
+const express=require("express");
+//import mongoDb
+const mongoDb=require("mongodb")
+//import cors
+const cors=require("cors")
+//import body-parser
+const bodyparser=require("body-parser");
+
+//create server
+const app=express();
+
+//app use cors and bodyparser
+app.use(cors())
+app.use(bodyparser.json())
+
+//mongo URL
+var mongoURL="mongodb://localhost:27017/PALLAVIdb";
+
+//now connect to mongo db
+mongoDb.MongoClient.connect(mongoURL,{useNewUrlParser:true,useUnifiedTopology:true},(err,client)=>{
+    //if error ocuur on connecting to db
+    if(err){
+        return console.log(err);
+    }
+    //if not occur
+    console.log("Connected to mongoDB");
+    const NewDB=client.db('PALLAVIdb');
+
+    //start creating api's from here
+
+    //post api create->signup
+    app.post("/signup",(request,response)=>{
+        //get name,number,email password from request body
+        const {name,phoneNumber,email,password}=request.body;
+        console.log(request.body);
+        
+        //check if email exist or not in db
+        //db.user.find //[{name:pallavi}]
+        let existingUser=NewDB.collection('user').find({email:email}).toArray()
+        existingUser.then(User=>{
+            //user already exist
+            if(User.length>0){
+                response.json("User Already exists")
+            }
+            else if(User.length<1){
+                let createdUser=NewDB.collection('user').insertOne(request.body);
+                createdUser.then(User=>{
+                    console.log("user=",User.ops);
+                    response.json(User.ops);
+                })
+            }
+        })
+    })
+})
+//start server
+app.listen(8081,()=>{
+    console.log("server started");
+})
+
